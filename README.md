502bd751baa7c509c034a3f35dc799298c13b4de
gregoriofrancisco99 <gregoriofrancisco2014@gmail.com>
2021-07-21 00:31:21 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# School site
+ A web app to manage a school class
---
+++ assets/js/funcionality.js
@@ -0,0 +1,9 @@
+const hover = {
+  mouseIn() {
+    //Select the desired tag and assign a class on it
+    document.querySelector('#news .fadding_caption').classList.add('active');
+  },
+  mouseOut() {
+    //Select the desired tag and remove a class from it
+  }
+}
