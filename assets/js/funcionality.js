bd66725f99957704955996d2a2b5a863bc8650a6
gregoriofrancisco99 <gregoriofrancisco2014@gmail.com>
2021-08-31 15:21:17 +0100

    Reboot

--- assets/js/funcionality.js
+++
@@ -1,9 +0,0 @@
-const hover = {
-  mouseIn() {
-    //Select the desired tag and assign a class on it
-    document.querySelector('#news .fadding_caption').classList.add('active');
-  },
-  mouseOut() {
-    //Select the desired tag and remove a class from it
-  }
-}
